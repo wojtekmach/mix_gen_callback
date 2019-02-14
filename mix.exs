2c09d3e8fe1670576387f71675247e0f086465ef
Wojtek Mach <wojtek@wojtekmach.pl>
2019-02-14 20:50:52 +0100

    Set version at 0.1.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GenCallback.MixProject do
   def project() do
     [
       app: :mix_gen_callback,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       docs: docs(),
